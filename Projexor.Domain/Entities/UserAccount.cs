779d3a6af1918deb606eb79d8bec731f91df4444
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-05-27 19:39:22 -0300

    Adicionado o ValueObject Password

--- Projexor.Domain/Entities/UserAccount.cs
+++ Projexor.Domain/Entities/UserAccount.cs
@@ -7,10 +7,12 @@ public class UserAccount : Entity
     public int Id { get; private set; }
     public Name Name { get; private set; }
     public Login Login { get; private set; }
+    public Password PasswordHash { get; private set; }
 
-    public UserAccount(string name, string login)
+    public UserAccount(string name, string login, string password)
     {
         Name = new Name(name);
         Login = new Login(login);
+        PasswordHash = new Password(password);
     }
 }
--- Projexor.Domain/ValueObjects/UserAccount/PasswordObject.cs
+++ Projexor.Domain/ValueObjects/UserAccount/PasswordObject.cs
@@ -1,6 +1,14 @@
+using Projexor.Domain.ExceptionExtension;
+
 namespace Projexor.Domain.ValueObjects;
 
 public class Password : ValueObject
 {
+    private string Value { get; set; }
 
+    public Password(string password)
+    {
+        DomainException.ThrowIfError(string.IsNullOrWhiteSpace(password), "Password não pode ser Vazio ou Nulo.");
+        Value = password;
+    }
 }
