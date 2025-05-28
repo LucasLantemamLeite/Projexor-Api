1d42fb428809a45e18595b452b418bd01ca7c30e
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-05-27 21:51:44 -0300

    Preparação da camada da API e injeção de dependência do Banco

---
+++ Projexor.Api/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Projexor.Data.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+app.Run();
